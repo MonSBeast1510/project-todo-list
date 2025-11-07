b82ed7d66f4ec990a741efc452deac0a73e26cf5
Nguyen IT <nguyen0915965863@gmail.com>
2025-11-07 14:42:45 +0700

    Fix: Áp dụng early return theo review

--- script.js
+++ script.js
@@ -7,18 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
  
     addButton.addEventListener('click', function() { 
         const taskText = taskInput.value; 
- 
-        if (taskText.trim() !== "") { 
-            // Tạo một <li> mới 
-            const newTask = document.createElement('li'); 
-            newTask.textContent = taskText; 
- 
-            // Thêm vào danh sách 
-            taskList.appendChild(newTask); 
- 
-            // Xóa nội dung input 
-            taskInput.value = ""; 
+     
+        // Áp dụng "early return" 
+        if (taskText.trim() === "") { 
+            alert("Vui lòng nhập nội dung!"); // Thêm tí bonus 
+            return;  
         } 
-    }); 
+     
+        // Tạo một <li> mới 
+        const newTask = document.createElement('li'); 
+        newTask.textContent = taskText; 
+     
+        // Thêm vào danh sách 
+        taskList.appendChild(newTask); 
+     
+        // Xóa nội dung input 
+        taskInput.value = ""; 
+    });  
 }); 
-○
